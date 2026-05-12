ba03fb49c5b92ba3a433d06505bc9165653d86f4
SebastianReilich <reilichsebastian245@student.voskh.cz>
2026-05-12 09:56:57 +0200

    Added more example data

--- data.ts
+++ data.ts
@@ -31,6 +31,30 @@ const products: ProductData[] = [
     basePrice: 7999,
     has5G: false,
   },
+  {
+    category: "Notebook",
+    name: "Apple MacBook Air M2",
+    basePrice: 26999,
+    ram: 16,
+  },
+  {
+    category: "Notebook",
+    name: "Acer Swift 5",
+    basePrice: 22999,
+    ram: 8,
+  },
+  {
+    category: "Phone",
+    name: "Google Pixel 7",
+    basePrice: 19999,
+    has5G: true,
+  },
+  {
+    category: "Phone",
+    name: "Nokia G22",
+    basePrice: 6999,
+    has5G: false,
+  },
 ];
 
 const catalog = products;
